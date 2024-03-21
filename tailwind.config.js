e315ab373ee0c925963eccad4bb19ce558bd0fe0
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-21 16:26:25 +0800

    Removed Flowbite TailwindCSS from project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 };
